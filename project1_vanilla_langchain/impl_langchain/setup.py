3a2f7cda615c4acf5e9d26723adcff3c37bdfc39
Hossam Hamdy Fakry Abdelhalim <210591705+hossamhamdy333@users.noreply.github.com>
2026-08-10 19:09:37 +0300

    Delete project1_vanilla_langchain directory

--- project1_vanilla_langchain/impl_langchain/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="rag_chatbot_langchain",
-    version="0.1.0",
-    packages=["src"],
-)
--- project1_vanilla_langchain/impl_vanilla/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="rag_chatbot_vanilla",
-    version="0.1.0",
-    packages=["src"],
-)
